ef1f1565ed2431f2fcdf46cd6774afd8e4bcbbc4
cdelashmutt-pivotal <cdelashmutt@pivotal.io>
2019-06-26 17:52:18 -0400

    initial

---
+++ src/main/java/io/pivotal/demo/simplereactive/SimpleReactiveApplication.java
@@ -0,0 +1,13 @@
+package io.pivotal.demo.simplereactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleReactiveApplication.class, args);
+	}
+
+}
